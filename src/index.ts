b4d6386e5abe5f81e66e7a3c3fb61064d16fa359
Raphael Stoeckli <github@rabanti.ch>
2017-07-19 23:48:42 +0200

    Dev-Commit 19.07.2017

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import List from './List';
 import {Dictionary} from './Dictionary';
-
 import {KeyValuePair} from './KeyValuePair';
+
+//export default List;
 
 /*
         let list2: List<Date> = new List<Date>([new Date(2015,2,10,0,0,0), new Date(2017,1,1,0,0,0), new Date(1191,1,8,23,59,59)]);
@@ -93,9 +94,9 @@ let output = d.get(4);
 
 
 
-
-export default List;
 export
 {
-    Dictionary   
+    List,
+    Dictionary,
+    KeyValuePair
 };
